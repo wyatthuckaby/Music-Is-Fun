c02f12a9b8f39c3b6bb56bdd374123dd229aaf58
Wyatt Huckaby <huckabywyatt@gmail.com>
2017-08-05 13:22:23 -0600

    slightly more cleanup

--- media-controller.js
+++ media-controller.js
@@ -2,12 +2,17 @@
 
 function MediaController() {
     this.musicPlayed = function(e) {
-        //aparently "target" is the clicked element - go figure
+
+    	/*
+    	This function is called onplay from any of the
+    	music players. Afterwards, all that needs to happen
+    	is find our way through the sometimes huge list
+    	of elements. then pause them.
+    	*/
         var players = document.getElementsByName("player");
         for (var i = players.length - 1; i >= 0; i--) {
-            if (players[i] != e) {
-                players[i].pause();
-            }
+			if (players[i]!=e) 
+				players[i].pause();
         }
     }
 }
