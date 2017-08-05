b1e6bf3461fb713ce8e68c2a90ba2c25ba804352
Wyatt Huckaby <huckabywyatt@gmail.com>
2017-08-05 12:59:36 -0600

    cleaned up some of the line spacing

--- media-controller.js
+++ media-controller.js
@@ -1,13 +1,13 @@
 /* exported MediaController */
 
 function MediaController() {
-	this.musicPlayed = function(e){
-		//aparently "target" is the clicked element - go figure
-		var players = document.getElementsByName("player");
-		for (var i = players.length - 1; i >= 0; i--) {
-			if (players[i] != e){
-				players[i].pause();
-			}
-		}
-	}
+    this.musicPlayed = function(e) {
+        //aparently "target" is the clicked element - go figure
+        var players = document.getElementsByName("player");
+        for (var i = players.length - 1; i >= 0; i--) {
+            if (players[i] != e) {
+                players[i].pause();
+            }
+        }
+    }
 }
